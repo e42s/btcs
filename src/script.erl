f1fe206ed7e6fb3d9d651eaf04665961ce904506
Ning Zhang <ning.m.zhang@gmail.com>
2013-06-23 12:26:59 -0400

    update script.erl

--- src/script.erl
+++ src/script.erl
@@ -1,6 +1,15 @@
 -module(script).
 -compile(export_all).
 
+%% Bitcoin uses a scripting system for transactions. Forth-like,
+%% Script is simple, stack-based, and processed from left to right. It
+%% is purposefully not Turing-complete, with no loops.
+%%
+%% Scripts are big-endian. 
+%% 
+%% Ref:
+%%   https://en.bitcoin.it/wiki/Script
+
 go() ->
     ok.
 
